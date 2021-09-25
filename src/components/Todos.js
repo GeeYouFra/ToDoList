f9274bb0e98645485019db70ff15d7936cdb8846
Giovani Francisco <giovani.francisco@myy.haaga-helia.fi>
2021-09-25 15:37:24 +0300

    ToDoList with Delete

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -11,6 +11,12 @@ function Todos() {
     const inputChanged = (event) => {
         setTodo({ ...todo, [event.target.name]: event.target.value })
     }
+
+    const deleteToDo = (row) => {
+        console.log('Row ' + row + ' deleted');
+        setTodos(todos.filter((todo, index) => index !== row));
+    }
+
     return (
         <div>
             <h2> Simple ToDo List </h2>
@@ -30,6 +36,7 @@ function Todos() {
                                 <tr key={index}>
                                     <td>{todo.desc}</td>
                                     <td>{todo.date}</td>
+                                    <td><button onClick={() => deleteToDo(index)}>Delete</button></td>
                                 </tr>)
                         }
                     </tbody>
