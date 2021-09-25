35ef2f20d8adb6ce45ee59c0b65e873aba516b8e
Giovani Francisco <giovani.francisco@myy.haaga-helia.fi>
2021-09-25 14:31:31 +0300

    ToDoList First

---
+++ src/components/Todos.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+function Todos() {
+    const [todo, setTodo] = useState({ desc: '', date: '' });
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = () => {
+        setTodos([todo, ...todos]);
+    }
+
+    const inputChanged = (event) => {
+        setTodo({ ...todo, [event.target.name]: event.target.value })
+    }
+    return (
+        <div>
+            <h2> Simple ToDo List </h2>
+            <p> Add todo: </p>
+            <div>
+                <label> Description: <input name="desc" value={todo.desc} onChange={inputChanged} /> </label>
+                <label> Date: <input name="date" value={todo.date} onChange={inputChanged} /> </label>
+                <button onClick={addTodo}>Add</button>
+                <table>
+                    <tbody>
+                        <tr><th> Date </th>
+                            <th> Description </th>
+                        </tr>
+                        {
+                            todos.map((todo, index) =>
+                                <tr key={index}>
+                                    <td>{todo.desc}</td>
+                                    <td>{todo.date}</td>
+                                </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    );
+}
+
+export default Todos;
